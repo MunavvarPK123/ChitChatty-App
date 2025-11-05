48ee302a716a1bb707618afc0ac6baa880b6a4a2
Munavvar P K <munavvarmunna780@gmail.com>
2025-11-05 19:06:38 +0530

    updated index

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -3,7 +3,23 @@ import axios from "axios";
 export const url = "https://chitchatty-app-server.onrender.com";
 
 export const axiosInstance = axios.create({
-    headers:{
-        authorization : `Bearer ${localStorage.getItem('token')}`
-    }
+    baseURL: url,
+    timeout: 15000,
 });
+
+// Request Interceptor to dynamically set the Authorization header
+axiosInstance.interceptors.request.use(
+    (config) => {
+        // Read the token every time a request is made
+        const token = localStorage.getItem('token'); 
+        
+        // If a token exists, add it to the Authorization header
+        if (token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
--- server/app.js
+++ server/app.js
@@ -7,33 +7,23 @@ const userRouter = require('./controllers/userController')
 const chatRouter = require('./controllers/chatController')
 const messageRouter = require('./controllers/messageController') 
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-// CORS configuration
-const allowedOrigin = 'https://chitchatty-app-client.onrender.com';
-const corsOptions = {
-    origin: allowedOrigin,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // allow all needed methods
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
 
-// Enable CORS
-app.use(cors(corsOptions));
+// Create server
+const server = http.createServer(app);
+
+const io = require('socket.io')(server, { cors:{
+    origin: ['https://chitchatty-app-client.onrender.com', 'http://localhost:3000'],
+    methods: ['GET', 'POST']
+}})
 
 // API routes
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/chat', chatRouter);
 app.use('/api/message', messageRouter);
-
-// Create server
-const server = http.createServer(app);
-
-// Socket.io setup
-const io = require('socket.io')(server, {
-    cors: corsOptions
-});
 
 // Keep track of online users
 const onlineUser = [];
@@ -43,7 +33,7 @@ io.on('connection', socket => {
         socket.join(userId);
     });
 
-    socket.on('send_msg', message => {
+    socket.on('send_msg', (message) => {
         io.to(message.members[0]).to(message.members[1]).emit('recv_msg', message);
         io.to(message.members[0]).to(message.members[1]).emit('set_msg_count', message);
     });
@@ -57,19 +47,14 @@ io.on('connection', socket => {
     });
 
     socket.on('user_login', userId => {
-        if (!onlineUser.includes(userId)) onlineUser.push(userId);
+        if (!onlineUser.includes(userId)) 
+            onlineUser.push(userId);
         socket.emit('all_online_users', onlineUser);
-        socket.broadcast.emit('online_users_updated', onlineUser);
     });
 
     socket.on('user-logout', userId => {
-        const index = onlineUser.indexOf(userId);
-        if (index > -1) onlineUser.splice(index, 1);
+        onlineUser.splice(onlineUser.indexOf(userId), 1);
         io.emit('online_users_updated', onlineUser);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Socket disconnected:', socket.id);
     });
 });
 
